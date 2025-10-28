5bb34d11af8df0208dffe7b2af29007d38cf9961
Martin <mardanmiller@gmail.com>
2025-10-27 22:02:29 -0500

    country list page

--- dynamic_server.mjs
+++ dynamic_server.mjs
@@ -52,6 +52,69 @@ app.get('/countries', (req, res) => {
     });
 }); 
 
+app.get('/countries', (req, res) => {
+    db.all('SELECT DISTINCT country FROM Powerplants ORDER BY country', (err, rows) => {
+        if (err) {
+            res.status(500).type('txt').send('SQL Error');
+        } else {
+            fs.readFile(path.join(template, 'list-pages.html'), 'utf-8', (err, data) => {
+                if (err) {
+                    res.status(500).type('txt').send('File Error');
+                } else {
+                    let countryList = '';
+                    for (const country of rows) {
+                        const countryName = country.country;
+                        const slug = countryName.replace(/ /g, '-').toLowerCase();
+                        countryList += `<li><a href="/countries/${slug}">${countryName}</a></li>\n`;
+                    }
+                    let page = data.replace('%%title%%', 'Countries');
+                    page = page.replace('%%list%%', countryList);
+                    res.status(200).type('html').send(page);
+                }
+            });
+        }
+    });
+}); 
+
+app.get('/powerplants', (req, res) => {
+    db.all('SELECT DISTINCT fuel1 FROM Powerplants ORDER BY fuel1', (err, rows) => {
+        if (err) {
+            res.status(500).type('txt').send('SQL Error');
+        } else {
+            fs.readFile(path.join(template, 'list-pages.html'), 'utf-8', (err, data) => {
+                if (err) {
+                    res.status(500).type('txt').send('File Error');
+                } else {
+                    let powerplantList = '';
+                    for (const powerplant of rows) {
+                        const powerplantType = powerplant.fuel1;
+                        const slug = powerplantType.replace(/ /g, '-').toLowerCase();
+                        powerplantList += `<li><a href="/powerplants/${slug}">${powerplantType}</a></li>\n`;
+                    }
+                    let page = data.replace('%%title%%', 'Powerplants');
+                    page = page.replace('%%list%%', powerplantList);
+                    res.status(200).type('html').send(page);
+                }
+            });
+        }
+    });
+}); 
+
+app.get('/power-capacities', (req, res) => {
+    fs.readFile(path.join(template, 'list-pages.html'), 'utf-8', (err, data) => {
+        if (err) {
+            res.status(500).type('txt').send('File Error');
+        } else {
+            const capacityLevels = '<li><a href="/power-capacities/low">Low</a></li>' +
+                                 '<li><a href="/power-capacities/medium">Medium</a></li>' +
+                                 '<li><a href="/power-capacities/high">High</a></li>';
+            let page = data.replace('%%title%%', 'Capacity Levels');
+            page = page.replace('%%list%%', capacityLevels);
+            res.status(200).type('html').send(page);
+        }
+    });
+}); 
+
 app.listen(port, () => {
     console.log('Now listening on port ' + port);
 }); 
