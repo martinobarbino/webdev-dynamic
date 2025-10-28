d8eabbeb387e01387ef54ec1809fd8e1e47484c5
Martin <mardanmiller@gmail.com>
2025-10-27 19:27:29 -0500

    package.json

--- dynamic_server.mjs
+++ dynamic_server.mjs
@@ -14,6 +14,25 @@ const template = path.join(__dirname, 'templates');
 let app = express();
 app.use(express.static(root));
 
+const db = new sqlite3.Database('./db.sqlite3', sqlite3.OPEN_READONLY, (err) => {
+        if (err)
+            console. log( 'Error connecting to database');
+        else 
+            console. log('Successfully connected to database');
+        
+});
+
+ app.get('/', (req, res) => {
+        db.all('SELECT * FROM Powerplants', (err, rows) => {
+            if (err) {
+                res.status(500).type('txt').send('SQL Error');
+            }
+            else {
+                res.status(200).type('json').send(JSON.stringify(rows));
+            }
+        });
+    });
+
 app.listen(port, () => {
     console.log('Now listening on port ' + port);
-});
+}); 
